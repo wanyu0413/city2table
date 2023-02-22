1b0d0d9393a4fec38d3b9bbf26bc4493b5971dbd
Julien Afonso <ju.afonso61@gmail.com>
2023-02-22 12:26:40 +0900

    pundit-seeds-view

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,7 @@
 class CoursesController < ApplicationController
+  skip_before_action :authenticate_user!, only:[:index, :show]
   def index
-    @courses = Course.all
+    @courses = policy_scope(Course)
   end
 
   def show
