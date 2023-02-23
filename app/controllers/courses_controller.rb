50a8c9cbfd936f10de60d3bab84ec64aa690c856
Wanyu Jiang <wanyu19930930@gmail.com>
2023-02-23 13:08:52 +0900

    user can create a new course now!

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -10,4 +10,26 @@ class CoursesController < ApplicationController
     authorize @course
     @booking = Booking.new
   end
+
+  def new
+    @course = Course.new
+    authorize @course
+  end
+
+  def create
+    @course = Course.new(course_params)
+    @course.user = current_user
+    authorize @course
+    if @course.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:category, :price, :name)
+  end
 end
