e6102dfd89eb2cda331ac5a3e9f70f65cc76681c
Julien Afonso <ju.afonso61@gmail.com>
2023-02-23 16:30:35 +0900

    status change

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,5 @@ class Booking < ApplicationRecord
   belongs_to :course
   belongs_to :user
   validates :start_time, presence: true
-  enum status: { pending: "pending", accepted: "accepted", rejected: "rejected" }
+  enum status: { pending: 0, accepted: 1, rejected: 2 }
 end
